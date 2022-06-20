21d55baadb9903816c750c3b03af276400d2c0a0
Greg Khmara <gkhmara@gmail.com>
2022-06-19 17:54:45 -0700

    update integration specs

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 describe "add a product to the page" do
   before :each do
+    visit new_user_registration_path
+    fill_in 'Email', with: 'fakename@fake.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_on 'Sign up'
+    User.find_by(email: "fakename@fake.com").update!(admin: true)
     visit new_product_path
     product = Product.create({name: "Meat Pie", cost: 3, country_of_origin: 'USA'})
-    click_on "Create Product"
+    click_on "Submit"
   end
 
   it "adds a new review" do
@@ -14,7 +20,7 @@ describe "add a product to the page" do
     fill_in 'Author', :with => 'Greg'
     fill_in 'Content body', :with => 'This product is great!'
     fill_in 'Rating', :with => 5
-    click_on 'Create Review'
+    click_on 'Submit'
     expect(page).to have_content 'Review successfully added!'
     expect(page).to have_content 'Greg'
   end
@@ -23,7 +29,7 @@ describe "add a product to the page" do
     visit products_path
     click_on "Meat Pie", match: :first
     click_on 'Add a review'
-    click_on 'Create Review'
+    click_on 'Submit'
     expect(page).to have_content "Author can't be blank"
   end
 
